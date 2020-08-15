55c3aa12c729d68cef3d89cfdd7ce2b694cd09f8
ryanbrooks99 <ryanbrooks99@gmail.com>
2020-08-14 23:14:57 -0400

    Revised html and JS based on TA consult

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,62 @@
-function pageStart() {
+var apiKey = "be4ba16383051ea85211fa8b62bcb28a"
 
-  // Setting variables 
-  var inputEl = $("city-input");
-  var searchEl = $("search-button");
-  var clearHistoryEl = $("clear-history");
-  var nameEl = $("city-name");
-  var picEl = $("current-pic");
-  var tempEl = $("temperature");
-  var humidityEl = $("humidity");
-  var windEl = $("wind-speed");
-  var UVIndexEl = $("UV-index");
-  var historyEl = $("history");
-  
-  var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-  
-  // console.log(searchHistory);
-  
-  
-  
-  searchEl.on("click", function (cityName) {
-    var APIkey = "be4ba16383051ea85211fa8b62bcb28a";
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey;
-    console.log(queryURL)
-  })
-  
-  
+var prevSearch = JSON.parse(localStorage.getItem("weather")) || []
+console.log(prevSearch)
 
-
-
-
-
-
+function showWeather(city) {
+    $.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey)
+        .then(function (res) {
+            $("#currentWeather").empty()
+            console.log(res)
+            var jumbotron = $("<div>").addClass("jumbotron ")
+            var cityName = $("<h2>").text(res.name + " (" + moment.unix(res.dt).format('L') + ')')
+            var img = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + res.weather[0].icon + ".png")
+            cityName.append(img)
+            var tempText = $("<div>").text("Temperature: " + res.main.temp + " F°")
+            var humidity = $("<div>").text("Humidity: " + res.main.humidity + " %")
+            var wind = $("<div>").text("Wind Speed: " + res.wind.speed + "")
+            $.get("https://api.openweathermap.org/data/2.5/uvi?lat=" + res.coord.lat + "&lon=" + res.coord.lon + "&appid=" + apiKey)
+                .then(function (uvRes) {
+                    var uv = $("<div>").text("UV Index: " + uvRes.value)
+                    jumbotron.append(cityName, tempText, humidity, wind, uv)
+                    $("#currentWeather").append(jumbotron)
+                })
+        })
+            
+        
+    for (let i = 0; i < 5; i++) {
+        var forecast = $.get("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey)
+        console.log(forecast)
+        var card = $("<div>").addClass("card bg-primary col-md-2 text-light")
+        var date = $("<div>").text("Date :")
+        var temp = $("<div>").text("Temp: " )
+        var humidity = $("<div>").text("Humidity: ")
+        card.append(date, temp, humidity)
+        $("#futureWeather").append(card)
+    }
+}       
+function renderList() {
+    $("#citySearch").empty()
+    for (let i = 0; i < prevSearch.length; i++) {
+        var div = $("<div>").text(prevSearch[i])
+        div.addClass("past text-center py-1 bg-light mb-1")
+        div.attr("data-city", prevSearch[i])
+        $("#citySearch").prepend(div)
+    }
 }
-
-pageStart();
-
+$(".btn-primary").on("click", function () {
+    var city = $("#cityName").val()
+    prevSearch.push(city)
+    localStorage.setItem("weather", JSON.stringify(prevSearch))
+    renderList()
+    showWeather(city)
+})
+$("#citySearch").on("click", ".past", function () {
+    var prevCityName = $(this).attr("data-city")
+    showWeather(prevCityName)
+})
+function init() {
+    showWeather(prevSearch[prevSearch.length - 1])
+}
+init()
+renderList()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Weather Dashboard
+
+# This weather dashboard works using the OpenWeather API to retreive weather data for the user. The API returns third party data such as city temperature, wind, and heat index. Local storage stores the user's previously visited cities on the left side column. User may click on the city previously earched to re-view weather data for that city.
+
+![alt text](Assets/weather.png)
